cca3792d4c25c9b0e96004b6b03665c69674e2dc
WhyskyConCafe <darkcray111@gmail.com>
2025-05-15 08:31:58 -0600

    Prueba de hola mundo

--- Codigo/main.py
+++ Codigo/main.py
@@ -1,17 +1,1 @@
-import mysql.connector
-
-def getConnection():
-    config={
-        'user': 'root',
-        'password': '',
-        'host': 'localhost',
-        'database': 'medicalbase',
-        'raise_on_warnings': True
-    }
-
-    try: 
-        connection = mysql.connector.connect(**config)
-        return connection
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-        return None
+print('hola mundo')
